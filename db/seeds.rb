3c2451781166de52a56ebd0ab97816c67a047781
ss473901 <satoshi.473901.1205@gmail.com>
2021-10-08 09:51:20 +0000

    Initial Commit

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Album.create(name: "崩壊アンプリファー")
+Album.create(name: "君繋ファイブエム")
+Album.create(name: "ソルファ")
+Album.create(name: "ファンクラブ")
+Album.create(name: "ワールドワールドワールド")
+Album.create(name: "サーフブンガクカマクラ")
+Album.create(name: "マジックディスク")
+Album.create(name: "ランドマーク")
+Album.create(name: "WonderFuture")
+Album.create(name: "ホームタウン")
